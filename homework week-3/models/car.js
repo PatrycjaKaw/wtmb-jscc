22300fd6ce6a940d424aafaa8a03c6eec7058299
PatrycjaKaw <56674722+PatrycjaKaw@users.noreply.github.com>
2019-11-11 17:59:59 +0100

    Delete car.js

--- homework week-3/models/car.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Car {
-    constructor(model, colour, productionDate){
-    this.model = model;
-    this.colour = colour;
-    this.productionDate = productionDate;
-    this.rentiers = []
-    this.buyers = []
-}
-}
